8674fbe4453f90e8642a69f180225fd287455c97
Munnaza <munnazashamim786@gmail.com>
2026-04-14 20:59:58 +0500

    robots.ts disallow

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,6 +1,17 @@
 import BlogPage from "@/src/components/blog/BlogsPage";
 import { FloatingWhatsapp } from "@/src/components/Whatsapp";
 import { languages } from "@/src/i18n/settings";
+import { Metadata } from "next";
+
+const baseurl = process.env.BASE_URL || "https://www.nexus-clinic.com"
+export const metadata: Metadata = {
+  title: "Nexus Clinic Blog – Aesthetic & Weight Loss Insights",
+  description: "MOH-certified clinic blog featuring expert advice on aesthetic, anti-aging, and weight loss treatments designed for safe, effective results.",
+  alternates: {
+    canonical: `${baseurl}/blogs`,
+  },
+};
+
 
 export async function generateStaticParams() {
   return languages.map((locale: string) => ({ locale }));
